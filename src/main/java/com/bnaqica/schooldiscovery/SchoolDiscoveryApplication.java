5e8d27bda66b279aed32b14fb6d557d447883f52
Brian Naqica <bnaqica@yahoo.com>
2019-11-25 23:08:28 -0700

    Skeleton of project commit

---
+++ src/main/java/com/bnaqica/schooldiscovery/SchoolDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.bnaqica.schooldiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnaqica/schooldiscovery/SchoolDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnaqica.schooldiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
