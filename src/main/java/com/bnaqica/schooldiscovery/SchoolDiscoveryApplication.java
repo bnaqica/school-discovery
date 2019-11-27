e08c2b5244b8749bc07c1ee239eee5fca95651db
Brian Naqica <bnaqica@yahoo.com>
2019-11-26 22:11:48 -0700

    Working Eureka Discovery/Registry server

--- src/main/java/com/bnaqica/schooldiscovery/SchoolDiscoveryApplication.java
+++ src/main/java/com/bnaqica/schooldiscovery/SchoolDiscoveryApplication.java
@@ -2,12 +2,14 @@ package com.bnaqica.schooldiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SchoolDiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchoolDiscoveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolDiscoveryApplication.class, args);
+    }
 
 }
